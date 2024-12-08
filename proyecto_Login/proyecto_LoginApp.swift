5af2bf50bc2c59edb087af6feba1ba167d6125f9
victortejeda <victorfernandotejeda@gmail.com>
2024-12-07 22:02:45 -0400

    Initial Commit

---
+++ proyecto_Login/proyecto_LoginApp.swift
@@ -0,0 +1,17 @@
+//
+//  proyecto_LoginApp.swift
+//  proyecto_Login
+//
+//  Created by Victor Tejeda on 7/12/24.
+//
+
+import SwiftUI
+
+@main
+struct proyecto_LoginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
